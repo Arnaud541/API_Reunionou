381cf777d69dbc4eecd0ebd42641c8d5a54df35b
StarCobra <damienpoirot@gmail.com>
2023-04-03 09:45:52 +0200

    Feature: API Backend

--- src/models/User.php
+++ src/models/User.php
@@ -5,6 +5,6 @@ namespace reunionou\models;
 class User extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = 'users';
-    protected $fillable = ['firstname', 'lastname', 'email', 'password'];
+    protected $fillable = ['firstname', 'lastname', 'avatar', 'email', 'password'];
     public $timestamps = false;
 }
